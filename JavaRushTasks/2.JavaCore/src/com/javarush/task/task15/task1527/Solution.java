5274389a612be397e8010710dc5c2231a0c08bc6
Kholmansky Maxim <maximkholmansky@gmail.com>
2018-01-23 22:13:52 +0700

    second commit

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task15.task1527;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+/* 
+Парсер реквестов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //add your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String url = reader.readLine();
+        String params = url.substring(url.indexOf("?")+1);
+        String[] splitParts = params.split("&");
+        Map<String, String> map = new HashMap<>();
+        for(String parts : splitParts){
+            parts.split("=");
+
+
+        }
+
+
+
+
+    }
+
+    public static void alert(double value) {
+        System.out.println("double " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String " + value);
+    }
+}
